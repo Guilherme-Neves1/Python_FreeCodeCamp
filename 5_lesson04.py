81ab410a1c7a55b5911a77398ce0465546e6e9a9
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-25 08:42:36 -0300

    lesson 04

--- 5_lesson04.py
+++ 5_lesson04.py
@@ -50,3 +50,16 @@ print(first)
 print(first.lower())
 print(first.upper())
 print(first)
+
+print(multiline.title())
+print(multiline.replace("good", "ok"))
+print(multiline)
+
+print(len(multiline))
+multiline += "                                        "
+multiline = "                  " + multiline
+print(len(multiline))
+
+print(len(multiline.strip()))
+print(len(multiline.lstrip()))
+print(len(multiline.rstrip()))
