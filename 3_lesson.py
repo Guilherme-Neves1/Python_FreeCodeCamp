cd8b5097c36a402fc4ff9f80eeed7e4678befb5c
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-24 21:12:39 -0300

    course python

---
+++ 3_lesson.py
@@ -0,0 +1,22 @@
+# Operadores
+
+# Adiçao(+)                        2 + 2 = 4
+# Subtração(-)                     4 - 2 = 2
+# Multiplicação(*)                 2 * 2 = 4
+# Divisão(/)                       24 / 5 = 4.8
+# Divisão arred. baixo(//)         24 // 5 = 4
+# Divisão arred. cima(round())     round(24 / 5) = 5
+# Resto da div(%)                  24 % 5 = 4
+# Potenciação(**)                  2 ** 3 = 8
+
+# Modificando o valor da variável com operações
+# meaning = 42
+# meaning += 1         # = 43
+# meaning += 1         # = 44
+# meaning -= 1         # = 43
+# meaning *= 10        # = 430
+# meaning /= 10        # = 43.0
+# round(meaning) = 43  # Tirar o decimal
+# meaning = 43.0
+# meaning = round(meaning)
+# meaning = 43
