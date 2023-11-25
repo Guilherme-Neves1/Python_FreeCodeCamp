317c3dba560ef6289344e01f9a4489391b659825
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-24 21:37:04 -0300

    lesson 4

--- 5_lesson04.py
+++ 5_lesson04.py
@@ -18,3 +18,35 @@ last = "Gray"
 # Concatenation
 fullname = first + " " + last
 print(fullname)
+
+fullname += "!"
+print(fullname)
+
+# Casting a number to a string
+decade = str(1980)
+print(type(decade))
+print(decade)
+
+statement = "I like rock music from the " + decade + "s."
+print(statement)
+
+# Multiples lines
+multiline = '''
+Hey, how are you?
+
+I was just checking in.
+                              All good?
+
+'''
+print(multiline)
+
+# Escaping special characters
+sentence = 'I\'m back at work!\tHey!\n\nWhere\'s this at\\located?'
+print(sentence)
+
+# String Methods
+
+print(first)
+print(first.lower())
+print(first.upper())
+print(first)
