a5f93599dbe0f24205288f2bd8e57fd36fefdba3
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-25 19:49:12 -0300

    update lesson06

--- 7_lesson06.py
+++ 7_lesson06.py
@@ -85,14 +85,27 @@ print(type(nums))
 mylist = list([1, 'Neil', True])
 print(mylist)
 
+# Tuples
 
+mytuple = tuple(('Dave', 42, True))
 
+anothertuple = (1, 4, 2, 8, 2, 2)
 
+print(mytuple)
+print(type(mytuple))
+print(type(anothertuple))
 
+newlist = list(mytuple)
+newlist.append('Neil')
+newtuple = tuple(newlist)
+print(newtuple)
 
+(one, *two, hey) = anothertuple
+print(one)
+print(two)
+print(hey)
 
-
-
+print(anothertuple.count(2))
 
 
 
