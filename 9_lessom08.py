0dedc7f71d69979b0967405d1ab37a987ec8d030
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-26 18:24:08 -0300

    lessson 08

---
+++ 9_lessom08.py
@@ -0,0 +1,37 @@
+# Loops
+value = 1
+# while value <= 10:
+#   print(value)
+#   value += 1
+
+# Break
+# print("")
+# while value <= 10:
+#   print(value)
+#   if value == 5:
+#     break
+#   value += 1
+
+# Continue
+# print("")
+# while value <= 10:
+#   value += 1
+#   if value == 5:
+#     continue
+#   print(value)
+# else:
+#   print("Value is now equal to " + str(value))
+
+names = ["Dave", "Sara", "John"]
+# for x in names:
+#   print(x)
+
+# for x in "Mississippi":
+#   print(x)
+
+for x in names:
+  if x == "Sara":
+    break
+  print(x)
+
+
