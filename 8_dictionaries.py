a0d66849d3db272f4f856e9231bf14fff91f3f41
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-26 17:56:01 -0300

    lesson 08

--- 8_dictionaries.py
+++ 8_dictionaries.py
@@ -112,4 +112,31 @@ band = {
 print(band)
 print(band["member1"]["name"])
 
+# Sets
+
+nums = { 1, 2, 3, 4}
+
+nums2 = set((1,2,3,4))
+
+print(nums)
+print(nums2)
+print(type(nums))
+print(len(nums))
+
+# No duplicate allowed => Não aparecem valores duplicados
+nums = { 1, 2, 2, 3}
+print(nums)
+
+# True is a dupe of 1, False is a dupe of zero
+nums = {1, True, 2, False, 3, 4, 0} # 1 é considerado True e 0 é considerado False
+print(nums)
+
+# Check if a value is in a set
+print(2 in nums)
+
+# but you cannot refer to an element in the set with an index position or a key
+
+# Add a new element to a set
+nums.add(8)
+print(nums)
 
