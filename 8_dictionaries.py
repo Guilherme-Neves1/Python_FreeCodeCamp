a18c3a5776dc3829ac9f384042dab0c3a4d20419
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-26 18:00:44 -0300

    lesson 08

--- 8_dictionaries.py
+++ 8_dictionaries.py
@@ -140,3 +140,31 @@ print(2 in nums)
 nums.add(8)
 print(nums)
 
+# Add elements from one set to another
+morenums = {5, 6, 7}
+nums.update(morenums)
+print(nums)
+
+# You can use update with lists, tuples, and dictionaries, too.
+
+# Merge two sets to crate a new set
+one = {1, 2, 3}
+two = {5, 6, 7}
+
+mynewset = one.union(two)
+print(mynewset)
+
+# Keep only the duplicates
+one = {1, 2, 3}
+two = {2, 3, 4}
+
+one.intersection_update(two)
+print(one)
+
+# Keep everything except the duplicates
+one = {1, 2, 3}
+two = {2, 3, 4}
+
+one.symmetric_difference_update(two)
+print(one)
+
