4b9504ed5b0152eeaea7c6a0150948b60f6e2596
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-26 19:24:51 -0300

    rps v2

--- 9_lessom08.py
+++ 9_lessom08.py
@@ -29,9 +29,42 @@ names = ["Dave", "Sara", "John"]
 # for x in "Mississippi":
 #   print(x)
 
-for x in names:
-  if x == "Sara":
-    break
+# for x in names:
+#   if x == "Sara":
+#     break
+#   print(x)
+
+# for x in names:
+#   if x == "Sara":
+#     continue
+#   print(x)
+
+# for x in range(4):
+#   print(x)
+
+# for x in range(2, 4):
+#   print(x)
+
+for x in range(5, 101, 5):
   print(x)
+else:
+  print("Glad that\'s over!")
+
+names = ["Dave", "Sara", "John"]
+actions = ["codes", "eats", "sleeps"]
+
+# for name in names:
+#   for action in actions:
+#     print(name + " " + action + ".")
+
+for action in actions:
+  for name in names:
+    print(name + " " + action + ".")
+
+
+
+
+
+
 
 
