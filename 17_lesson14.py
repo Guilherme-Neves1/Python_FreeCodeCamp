0444134ca9e7c1a7c979406bce754a652218ac48
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-12-23 12:47:12 -0300

    rpsv7

--- 17_lesson14.py
+++ 17_lesson14.py
@@ -1,5 +1,18 @@
 # MODULES
 
+# import math
+from math import pi
 
+import sys
+import random as rdm
+from enum import Enum
 
+# print(math.pi) => import math
+print(pi)
 
+# rdm.choice("123")
+
+for item in dir(rdm):
+  print(item)
+
+print(__name__)
