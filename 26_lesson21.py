fce227d19e7746ad21c4ba153d2aeaad20f8f195
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-12-26 19:03:28 -0300

    lesson 21

---
+++ 26_lesson21.py
@@ -0,0 +1,16 @@
+# COMANDOS PIP
+
+# python -m pip install 'BIBLIOTECA' => Instala globalmente
+## ex.: python -m pip install requests
+
+# python -m pip list
+## Mostra todas bibliotecas instaladas pelo pip
+
+# python -m pip install request==2.30.0 => Instalando uma versão anterior
+
+# python -m pip install -U requests => Atualiza a versão
+
+# python -m pip uninstall requests => Desinstala biblioteca
+
+
+
